529e863dde41e94b39381bfdf96fa268387ca156
7hyunii <123531315+7hyunii@users.noreply.github.com>
2025-03-08 17:21:05 +0900

    [Silver III] Title: 등차수열을 만들어요, Time: 428 ms, Memory: 266904 KB -BaekjoonHub

--- 백준/Silver/32298. 등차수열을 만들어요/등차수열을 만들어요.py
+++ 백준/Silver/32298. 등차수열을 만들어요/등차수열을 만들어요.py
@@ -4,6 +4,7 @@ import math
 
 n, m = map(int, input().split())
 
+#에라 체가 핵심이었네
 check = [True] * 20000001
 check[0] = check[1] = False
 for i in range(2, int(math.sqrt(2000000)) + 1):
@@ -15,8 +16,8 @@ Tog = True
 for i in range(1, 2000001):
     arr = [i + j * m for j in range(n)]
 
-    if all(not check[num] for num in arr):  
-        print(*arr)  # 찾으면 즉시 출력
+    if all(not check[num] for num in arr):  #최악일 때 O(n)
+        print(*arr)
         quit()
 
 print(-1)
