81386dd491dc2069f2b9757e47ea7ae85543d1dc
Seonghyeon Mun <123531315+7hyunii@users.noreply.github.com>
2025-10-16 13:44:05 +0900

    [Silver II] Title: 에디터, Time: 344 ms, Memory: 57108 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,26 +1,25 @@
-import sys
-input=sys.stdin.readline
-from collections import deque
+import sys, math
+import heapq
+from collections import deque, defaultdict
+input = sys.stdin.readline
+
 s = input().rstrip()
-n = int(input())
-d1 = deque()
-d2 = deque()
+m = int(input())
 
-for i in s:
-    d1.append(i)
+s1 = deque(s)
+s2 = deque()
+for i in range(m):
+    cmd = input().split()
+    if cmd[0] == "P":
+        s1.append(cmd[1])
+    elif cmd[0] == "L":
+        if s1:
+            s2.appendleft(s1.pop())
+    elif cmd[0] == "D":
+        if s2:
+            s1.append(s2.popleft())
+    else:
+        if s1:
+            s1.pop()
 
-for _ in range(n):
-    a = input().split()
-    if a[0]=='L':
-        if d1:
-            d2.appendleft(d1.pop())
-    if a[0]=='D':
-        if d2:
-            d1.append(d2.popleft())
-    if a[0]=='B':
-        if d1:
-            d1.pop()     
-    if a[0]=='P':
-        d1.append(a[1])
-
-print(*d1, *d2, sep="")
+print(*s1, *s2, sep="")
