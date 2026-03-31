2aa1e4c8f186acbec885768437a70684e9ba873a
Seonghyeon Mun <123531315+7hyunii@users.noreply.github.com>
2026-03-31 09:42:42 +0900

    [Platinum V] Title: 부분배열 고르기, Time: 176 ms, Memory: 48424 KB -BaekjoonHub

---
+++ 백준/Platinum/2104. 부분배열 고르기/부분배열 고르기.py
@@ -0,0 +1,46 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+n = int(input())
+arr = list(map(int, input().split())) + [0]
+pf = [0]
+for i in range(n):
+    pf.append(arr[i] + pf[i])
+
+stack = []
+ans = 0
+for i in range(n+1):
+    while stack and arr[stack[-1]] > arr[i]:
+        x = arr[stack.pop()]
+
+        if stack:
+            l = stack[-1]
+        else:
+            l = -1
+
+        r = i
+        ans = max(ans, x * (pf[r] - pf[l+1]))
+        # print(x, l, r, " ", x, pf[r], pf[l+1], ans)
+    
+    stack.append(i)
+
+print(ans)
+        
+
+"""
+i~j prefix sum * min(i~j) = MAX_answer
+               
+3 1 6 4 5 2 0   0 3 4 10 14 19 21  
+
+idxStack    stack   pop     l   r   prob
+0           3       
+1           1       3       0   1   9  
+1 2         1 6     
+1 3         1 4     6       1   3   36
+1 3 4       1 4 5
+1 3 5       1 4 2   5       3   5   25             
+1 5         1 2     4       1   5   60
+
+
+"""
